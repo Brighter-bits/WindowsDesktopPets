5c430aa9c6a2ef6c8fe1359e8a28605f51e11141
Brighter-bits <85905618+Brighter-bits@users.noreply.github.com>
2024-11-06 15:17:26 +0000

    Completed the Right Click menu (note it doens't actually do anything right now)

--- Main.cpp
+++ Main.cpp
@@ -69,14 +69,20 @@ struct Textures { // Holds information of which sprite is being accessed; the di
         Index.push_back(&LR);
         Index.push_back(&UD);
     }
+    std::vector<SDL_Rect> Rects;
 };
 
+struct Logic {
+    SDL_Event event;
+    bool running = true;
+    bool ShowRClickMenu = false;
+};
 
 bool MakeTransparent(SDL_Window* window); // This makes the background transparent, allowing only the sprite to be visible
 void TextureInit(SDL_Renderer* renderer, Textures &Textures); // Creates the Textures Struct and puts all the information into it
-void Turn(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool &running); // Turn the sprite around and has them go in a different direction
-void Render(SDL_Renderer* renderer, SDL_Event &event, bool &running, Textures &Textures, WindowManager* windowow = nullptr); // The render function which slaps the sprite onto the window and renders everything
-void Vertical(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool &running, WindowManager &window); // Tells the sprite to go up or down
+void Turn(SDL_Renderer* renderer, Textures &Textures, Logic &logic); // Turn the sprite around and has them go in a different direction
+void Render(SDL_Renderer* renderer, Logic &logic, Textures &Textures, WindowManager* windowow = nullptr); // The render function which slaps the sprite onto the window and renders everything
+void Vertical(SDL_Renderer* renderer, Textures &Textures, Logic &logic, WindowManager &window); // Tells the sprite to go up or down
 
 int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
     SDL_VideoInit(NULL);
@@ -90,23 +96,20 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
     RandomInt rng(0, 100); // Random number generator between 1 and 100
     Textures Textures(64, 64); // Initialises Textures and tells the sprite dimensions
     WindowManager window(0, 0, Textures.SpriteHeight, Textures.SpriteWidth);
+    Logic logic;
 
-
-
-    SDL_SetWindowOpacity(window.window, 1);
     SDL_Renderer* renderer = SDL_CreateRenderer(window.window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Event event;
     TextureInit(renderer, Textures); //Puts the Sprites into Textures
 
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
     while (running) {
-        Render(renderer, event, running, Textures, &window); // Renders the window
+        Render(renderer, logic, Textures, &window); // Renders the window
         
 
         if (Timer % 20 == 0) {
             if (window.CoordX >= (window.Screenwidth-Textures.SpriteWidth) || window.CoordX <= -1) { // If the window touches the edge, turn
                 GoRight = !GoRight;
-                Turn(renderer, Textures, event, running);
+                Turn(renderer, Textures, logic);
             }
             if (GoRight) {
                 window.CoordX += 5;
@@ -127,7 +130,7 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
             }
             
             if ((rng.out() == 5) && (Timer % 200 == 0)) { // Random change for the Sprite to go up or down
-                Vertical(renderer, Textures, event, running, window);
+                Vertical(renderer, Textures, logic, window);
             }
 
         }
@@ -149,7 +152,7 @@ bool MakeTransparent(SDL_Window* window){
     return SetLayeredWindowAttributes(WindowHandle, RGB(1, 1, 1), 0, LWA_COLORKEY); // Turn a weird greying colour transparent, this means that that colour CANNOT be used in the sprite
 }
 
-void Turn(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool &running) {
+void Turn(SDL_Renderer* renderer, Textures &Textures, Logic &logic) {
     bool Complete = false;
     unsigned int Timer = 0;
     bool Up = true; //If Up is true, the Sprite index is increasing, else, decreasing
@@ -168,7 +171,7 @@ void Turn(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool &ru
         if (((Textures.SpriteIndex == ((*Textures.Index[Textures.VectorIndex]).size()-1)) && (Up == true) || ((Textures.SpriteIndex == 0) && (Up == false)))) {
             Complete = true;
         }
-        Render(renderer, event, running, Textures);
+        Render(renderer, logic, Textures);
 
         Timer += 1;
     }
@@ -212,27 +215,69 @@ void TextureInit(SDL_Renderer* renderer, Textures &Textures) {
     }
 }
 
-void Render(SDL_Renderer* renderer, SDL_Event &event, bool &running, Textures &Textures, WindowManager* windowow) {
+void Render(SDL_Renderer* renderer, Logic &logic, Textures &Textures, WindowManager* windowow) {
     WindowManager &window = *windowow;
-    while (SDL_PollEvent(&event) != 0) {
-        if (event.type == SDL_QUIT) {
-            running = false;
+    while (SDL_PollEvent(&logic.event) != 0) {
+        if (logic.event.type == SDL_QUIT) {
+            logic.running = false;
+        }
+        if (logic.event.type == SDL_MOUSEBUTTONDOWN) {
+            SDL_Point mouse = { logic.event.button.x, logic.event.button.y };
+            if (logic.event.button.button == SDL_BUTTON_RIGHT) {
+                SDL_Rect SetButton = { mouse.x, mouse.y, 140, 30 };
+                SDL_Rect QuitButton = { mouse.x, mouse.y + 30, 140, 30 };
+                SDL_Rect SetShadow = { SetButton.x + 5, SetButton.y + 5, SetButton.w, SetButton.h };
+                SDL_Rect QuitShadow = { QuitButton.x + 5, QuitButton.y + 5, QuitButton.w, QuitButton.h };
+                Textures.Rects.push_back(SetButton);
+                Textures.Rects.push_back(SetShadow);
+                Textures.Rects.push_back(QuitButton);
+                Textures.Rects.push_back(QuitShadow);
+                logic.ShowRClickMenu = true;
+                SDL_SetWindowSize(window.window, 400, 400);
+            } else if (logic.event.button.button == SDL_BUTTON_LEFT && logic.ShowRClickMenu) {
+                if (SDL_PointInRect(&mouse, &Textures.Rects[0])) {
+                    SDL_Log("OH MY GOD");
+                    logic.ShowRClickMenu = false;
+                }
+                else if (SDL_PointInRect(&mouse, &Textures.Rects[2])) {
+                    SDL_Log("ARGH");
+                    logic.running = false;
+                }
+                else {
+                    Textures.Rects = {};
+                    logic.ShowRClickMenu = false;
+                }
+            }
         }
     }
-    if (windowow != nullptr) { // If window is a null pointer then don't change the window, just update its contents
-        SDL_SetWindowPosition(window.window, window.CoordX, window.CoordY);
-        MakeTransparent(window.window);
-    }
-
     SDL_SetRenderDrawColor(renderer, 1, 1, 1, 0); // Make the background that horrible transparent shade of grey
     SDL_RenderClear(renderer);
+    if (logic.ShowRClickMenu) {
+        for (int i = 0; i < 3; i+=2) {
+            SDL_SetRenderDrawColor(renderer, 5, 5, 5, 175);
+            SDL_RenderFillRect(renderer, &Textures.Rects[i+1]);
+            SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+            SDL_RenderFillRect(renderer, &Textures.Rects[i]);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderDrawRect(renderer, &Textures.Rects[i]);
+        }
+    }
+    else {
+        if (windowow != nullptr) { // If window is a null pointer then don't change the window, just update its contents
+            SDL_SetWindowPosition(window.window, window.CoordX, window.CoordY);
+            MakeTransparent(window.window);
+        }
 
-    SDL_Rect destRect = { 0, 0, Textures.SpriteHeight, Textures.SpriteWidth}; // Create a rectangle which has the same dimensions as the textures
-    SDL_RenderCopy(renderer, (*Textures.Index[Textures.VectorIndex])[Textures.SpriteIndex], NULL, &destRect); // Slap on the texture to the rectangle
+
+
+    
+        SDL_Rect destRect = { 0, 0, Textures.SpriteHeight, Textures.SpriteWidth}; // Create a rectangle which has the same dimensions as the textures
+        SDL_RenderCopy(renderer, (*Textures.Index[Textures.VectorIndex])[Textures.SpriteIndex], NULL, &destRect); // Slap on the texture to the rectangle
+    }
     SDL_RenderPresent(renderer); // Render it
 }
 
-void Vertical(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool &running, WindowManager &window) {
+void Vertical(SDL_Renderer* renderer, Textures &Textures, Logic &logic, WindowManager &window) {
     int Complete = 0;
     unsigned int Timer = 0;
     RandomInt rng(0, 1);
@@ -257,12 +302,6 @@ void Vertical(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool
     Textures.SpriteIndex = middle;
     int count = 0;
     while (Complete <= 2) {
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
-                Complete = true;
-                running = false;
-            }
-        }
         if ((Timer % 20 == 0) && (Timer != 0)) {
             if (count == 0) { // If the counting hasn't started yet, change the textures until they are in the correct position and start the count
                 if (Timer % 300 == 0) {
@@ -309,7 +348,7 @@ void Vertical(SDL_Renderer* renderer, Textures &Textures, SDL_Event &event, bool
             }
         }
 
-        Render(renderer, event, running, Textures, &window); // Render the changes
+        Render(renderer, logic, Textures, &window); // Render the changes
         Timer += 1;
     }
     Textures.SpriteIndex = direction; //Put the Sprite index Back to position
